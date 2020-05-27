fedf4339baed16e66ea5e8930ffe961a21d9f140
joe-broder15 <joe.broder@berkeley.edu>
2020-05-26 17:06:14 -0700

    restructured project

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 7000
-const stripe = require('stripe')('YOUR STRIPE PRIVATE KEY HERE');
+const stripe = require('str')('YOUR STRIPE SECRET KEY');
 
 
 
